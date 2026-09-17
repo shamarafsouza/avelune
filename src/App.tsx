73c0e2836f7e33307769b66835d5d5b4cd230e0f
SHAMARA FERREIRA DE SOUZA <129136625+shamarafsouza@users.noreply.github.com>
2026-09-17 09:22:11 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import { useEffect, useRef, useState } from "react";
 import { criarCena } from "./three/Scene";
 import Biblioteca from "./pages/Biblioteca";
 import Comunidade from "./pages/Comunidade";
+import Grupos from "./pages/Grupos";
 import Perfil from "./pages/Perfil";
 import Auth from "./pages/Auth";
-import "./App.css";
 
 type Pagina =
   | "inicio"
